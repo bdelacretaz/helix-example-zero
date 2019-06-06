2d5ddbf05a4c267b64489cc2fc07f8472c78f6f5
Bertrand Delacretaz <bdelacretaz@apache.org>
2019-06-06 15:26:04 +0200

    Test setup

---
+++ test/test.helix.pages.output.js
@@ -0,0 +1,12 @@
+const assert = require('assert');
+
+describe('Testing the basic Helix Pages output', () => {
+    it('is true', () => {
+      assert.ok(true);
+    });
+  
+    it('is false', () => {
+        assert.ok(true);
+    });
+    
+  });
